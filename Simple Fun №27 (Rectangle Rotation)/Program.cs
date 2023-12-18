04b7d790fa98535ef39912e33aa340958bc9b2aa
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-12-18 10:07:28 +0300

    Добавьте файлы проекта.

---
+++ Simple Fun №27 (Rectangle Rotation)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace myjinxin
+{
+    public class Kata
+    {
+        public static void Main()
+        {
+            Kata test = new();
+
+            // Test
+            var t = test.RectangleRotation(6, 4);
+            // ...should return 23
+        }
+
+        readonly double cellLength = Math.Sqrt(2);
+
+        public int RectangleRotation(int a, int b)
+        {
+            return CalcFullCellsNumber(a) * CalcFullCellsNumber(b) + CalcHalfCellsNumber(a) * CalcHalfCellsNumber(b);
+        }
+
+        public int CalcFullCellsNumber(int n)
+        {
+            return (int)(n / 2d / cellLength) * 2 + 1;
+        }
+
+        public int CalcHalfCellsNumber(int n)
+        {
+            return ((int)((n / 2d - cellLength / 2) / cellLength) + 1) * 2;
+        }
+    }
+}
